5e9671dc72937f656a66d9b4a34945836f402ddd
syamsum <hisyamvcd@gmail.com>
2023-02-13 15:39:48 +0700

    menambahkan Networking Adapter

---
+++ app/src/main/java/com/example/relativelayout_hisyam_xiirpl/model/CategoriesModel.kt
@@ -0,0 +1,13 @@
+package com.example.relativelayout_hisyam_xiirpl.model
+
+class CategoriesModel : java.io.Serializable
+{
+    @JvmField
+    var strCategory : String? = null
+
+    @JvmField
+    var strCategoryThumb : String? = null
+
+    @JvmField
+    var strCategoryDescription : String? = null
+}
---
+++ app/src/main/java/com/example/relativelayout_hisyam_xiirpl/networking/API.kt
@@ -0,0 +1,6 @@
+package com.example.relativelayout_hisyam_xiirpl.networking
+
+object API {
+
+    var Categories = " https://gist.github.com/MasKoding/8cc351fa3a90aeac171fc4e5f18f47d3"
+}
