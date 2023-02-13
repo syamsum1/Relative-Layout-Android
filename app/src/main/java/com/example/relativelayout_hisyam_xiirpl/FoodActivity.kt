18f427cf7eb955ce00c1fdeefecdda8e005a0246
syamsum <hisyamvcd@gmail.com>
2023-02-13 10:53:33 +0700

    menambahkan Acticity Food

---
+++ app/src/main/java/com/example/relativelayout_hisyam_xiirpl/FoodActivity.kt
@@ -0,0 +1,11 @@
+package com.example.relativelayout_hisyam_xiirpl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food)
+    }
+}
